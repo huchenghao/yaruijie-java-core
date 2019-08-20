a71d6499629954363af48e659338df8d6fcdcd22
huchenghao <578849310@qq.com>
2019-08-20 13:46:36 +0800

    fileHelper

--- src/main/java/com/yrj/common/FileHelper.java
+++ src/main/java/com/yrj/common/FileHelper.java
@@ -1,10 +1,11 @@
 package com.yrj.common;
 
 import java.io.File;
-import java.math.BigDecimal;
 import java.util.UUID;
-public class FileHelper {/*
-	*//**
+
+import org.springframework.web.multipart.MultipartFile;
+public class FileHelper {
+	/**
 	 * 
 	 * @Title: uploadFile
 	 * @Description: 上传附件
@@ -13,7 +14,7 @@ public class FileHelper {/*
 	 * @return
 	 * @throws Exception
 	 * @author huchenghao
-	 *//*
+	 */
 	public static String uploadFile(MultipartFile enclosure,String filePath) throws Exception {
 		String fileType = getFileType( enclosure.getOriginalFilename());
 		String path =  filePath;
@@ -64,11 +65,11 @@ public class FileHelper {/*
 	}
 	
 	
-	*//**
+	/**
 	 * 获取文件的后缀
 	 * @param name
 	 * @return .jpeg
-	 *//*
+	 */
 	public static String getFileType(String name) {
 		if (name != null && !"".equals(name)) {
 			int index = name.lastIndexOf(".");
@@ -80,11 +81,11 @@ public class FileHelper {/*
 		}
 		return "";
 	}
-	*//**
+	/**
 	 * 获取文件的后缀
 	 * @param name
 	 * @return .jpeg
-	 *//*
+	 */
 	public static String getFileType_file(String name) {
 		if (name != null && !"".equals(name)) {
 			int index = name.lastIndexOf(".");
@@ -127,38 +128,5 @@ public class FileHelper {/*
             return false;  
         }  
     }  
-	*//**
-	 * 
-	 * @Title: convertFileUnit
-	 * @Description: 文件字节数转换为合适的单位
-	 * @author: 李国刚
-	 * @param size
-	 * @return
-	 * @return: String
-	 *//*
-	public static String convertFileUnit(long size) {
-		 // 如果字节数少于1024，则直接以B为单位，否则先除于1024，后3位因太少无意义  
-       double value = (double) size;  
-       if (value < 1024) {  
-           return String.valueOf(value) + "B";  
-       } else {  
-           value = new BigDecimal(value / 1024).setScale(2, BigDecimal.ROUND_DOWN).doubleValue();  
-       }  
-       // 如果原字节数除于1024之后，少于1024，则可以直接以KB作为单位  
-       // 因为还没有到达要使用另一个单位的时候  
-       // 接下去以此类推  
-       if (value < 1024) {  
-           return String.valueOf(value) + "KB";  
-       } else {  
-           value = new BigDecimal(value / 1024).setScale(2, BigDecimal.ROUND_DOWN).doubleValue();  
-       }  
-       if (value < 1024) {  
-           return String.valueOf(value) + "MB";  
-       } else {  
-           // 否则如果要以GB为单位的，先除于1024再作同样的处理  
-           value = new BigDecimal(value / 1024).setScale(2, BigDecimal.ROUND_DOWN).doubleValue();  
-           return String.valueOf(value) + "GB";  
-       }
-	}
 	
-*/}
+}
